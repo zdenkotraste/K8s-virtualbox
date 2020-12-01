1623e7e9d1fc85b0936eeb03f98a82bc294dd0b6
zdenkotraste <zhraste98@gmail.com>
2020-12-01 17:54:22 -0300

    k8s cluster ready with nginx service

---
+++ k8smaster/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+sudo apt-get update && sudo apt-get upgrade -y
+#Install docker
+sudo apt-get install -y docker.io
+#Add k8s to the apt repository
+sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list"
+#Add google pgp key
+sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
+sudo apt-get update
+sudo apt-get install -y kubeadm kubelet kubectl
+sudo kubeadm init --apiserver-advertise-address=192.168.100.10 --pod-network-cidr=192.168.100.0/24
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo sh -c "echo 'Environment=”KUBELET_EXTRA_ARGS=--node-ip=192.168.100.10”' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
---
+++ k8snode2/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt-get update && sudo apt-get upgrade -y
+#Install docker
+sudo apt-get install -y docker.io
+#Add k8s to the apt repository
+sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list"
+#Add google pgp key
+sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
+sudo apt-get update
+sudo apt-get install -y kubeadm kubelet kubectl
+sudo sh -c "echo 'Environment=”KUBELET_EXTRA_ARGS=--node-ip=192.168.100.12”' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
