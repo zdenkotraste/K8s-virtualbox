2f7ee7de27144a5fc233804594320c03b8c64aa1
zdenkotraste <zhraste98@gmail.com>
2020-12-15 19:45:11 -0300

    Entrega final - Testeado el funcionamiento

--- k8snode1/bootstrap.sh
+++ k8snode1/bootstrap.sh
@@ -8,6 +8,6 @@ sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/
 sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
 sudo apt-get update
 sudo apt-get install -y kubeadm kubelet kubectl
-sudo sh -c "echo 'Environment=”KUBELET_EXTRA_ARGS=--node-ip=192.168.100.11”' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
-sudo systemctl daemon-reload
-sudo systemctl restart kubelet
+#sudo sh -c "echo 'Environment=”KUBELET_EXTRA_ARGS=--node-ip=192.168.100.11”' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
+#sudo kubectl label nodes k8snode1 logstash=true kibana=true filebeat=true elasticsearch=false
